453e1630bb710e9582b99c97f5b0c87c2bd5a081
soojeonglee <eee.soojeong@gmail.com>
2021-07-19 21:44:15 +0900

    [Settings] 초기 설정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import theme from './styles/theme';
 
 // #ROUTES
 import Routes from './Routes';
@@ -9,12 +10,11 @@ import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 
 ReactDOM.render(
-    <>
-        <GlobalStyle/>
-        <ThemeProvider theme = {theme}>
-            <Routes />
-        </ThemeProvider>
-    </>,
-     document.getElementById('root')
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  </>,
+  document.getElementById('root')
 );
-
